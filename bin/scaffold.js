f5c486718db1c43697defd61c6b8eeb6e84f22b1
Fred Kim <yhk1038@hanmail.net>
2018-08-04 17:31:13 +0900

    Add command

--- bin/scaffold.js
+++ bin/scaffold.js
@@ -6,9 +6,26 @@ commander
     .arguments('<resource>')
     .option('-o, --only <scaffold_element>', 'Generate only one of (Controller | Model | Views).')
     .action(function(resource){
-        if (resource === 'install') {
-            cmd.run('npm install express-scaffold-mvc-base --save');
-        } else {
+        let core_dependencies = [
+            'express-scaffold-mvc-base' // core base classes of Model, Controller, View
+        ];
+        let dependencies = [
+            'ejs', 'express-ejs-layouts' // default template engine ~> ejs
+        ];
+
+        for(var i in core_dependencies) {
+            var pkg = core_dependencies[i];
+            dependencies.push(pkg);
+        }
+
+        if (resource === 'install' || resource == 'I') {
+            cmd.run(`npm i -S ${dependencies.join(' ')}`);
+        }
+        else if (resource === 'update' || resource == 'U') {
+            cmd.run(`npm i express-scaffold-mvc-generator -g -S && npm i -S ${core_dependencies.join(' ')}`);
+        }
+        
+        else {
             require('../scaffold')(resource);
         }
     })
